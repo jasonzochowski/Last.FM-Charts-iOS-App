0a9a2417ad6250d56c54f937cdd6f976806e36a5
Jason Zochowski <z1836771@cs305824.local>
2019-04-09 11:04:33 -0500

    Initial Commit

---
+++ ZochowskiLastFMCharts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ZochowskiLastFMCharts
+//
+//  Created by Jason Zochowski on 4/9/19.
+//  Copyright © 2019 Jason Zochowski. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ ZochowskiLastFMChartsTests/ZochowskiLastFMChartsTests.swift
@@ -0,0 +1,34 @@
+//
+//  ZochowskiLastFMChartsTests.swift
+//  ZochowskiLastFMChartsTests
+//
+//  Created by Jason Zochowski on 4/9/19.
+//  Copyright © 2019 Jason Zochowski. All rights reserved.
+//
+
+import XCTest
+@testable import ZochowskiLastFMCharts
+
+class ZochowskiLastFMChartsTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
